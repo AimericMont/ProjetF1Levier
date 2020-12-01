d2d87755706ae5e96f4e67db9c7ec08cde53e478
AimericMont <aimeric.montange@gmail.com>
2020-12-01 18:41:17 +0100

    interface run complete

--- app/src/main/java/com/example/projetf1levier/RunActivity.java
+++ app/src/main/java/com/example/projetf1levier/RunActivity.java
@@ -1,25 +1,28 @@
 package com.example.projetf1levier;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    int i=0;
-    @Override
+public class RunActivity extends AppCompatActivity {
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.run_screen);
     }
 
     public void onClick(View v) {
-        ImageView I = findViewById(R.id.imageView);
+        ImageView I = findViewById(R.id.num_player_team1);
         int[] Image={R.drawable.pict1,R.drawable.pict2,R.drawable.pict3};
-        I.setImageResource(Image[i]);
-        i++;
+        I.setImageResource(Image[1]);
     }
 }
+
+
+
+
 
