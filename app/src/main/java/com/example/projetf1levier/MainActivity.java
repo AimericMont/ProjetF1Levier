eaae7268f2bc36e6ac8535e884e6982c70ceb537
AimericMont <aimeric.montange@gmail.com>
2020-12-01 18:59:02 +0100

    Image 1-2-3 Switch

--- app/src/main/java/com/example/projetf1levier/MainActivity.java
+++ app/src/main/java/com/example/projetf1levier/MainActivity.java
@@ -3,23 +3,12 @@ package com.example.projetf1levier;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int i=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    public void onClick(View v) {
-        ImageView I = findViewById(R.id.imageView);
-        int[] Image={R.drawable.pict1,R.drawable.pict2,R.drawable.pict3};
-        I.setImageResource(Image[i]);
-        i++;
-    }
 }
-
--- app/src/main/java/com/example/projetf1levier/RunActivity.java
+++ app/src/main/java/com/example/projetf1levier/RunActivity.java
@@ -15,10 +15,11 @@ public class RunActivity extends AppCompatActivity {
         setContentView(R.layout.run_screen);
     }
 
+    private static int i=1;
     public void onClick(View v) {
         ImageView I = findViewById(R.id.num_player_team1);
         int[] Image={R.drawable.pict1,R.drawable.pict2,R.drawable.pict3};
-        I.setImageResource(Image[1]);
+        I.setImageResource(Image[i++]);
     }
 }
 
